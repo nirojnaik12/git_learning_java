3e1a0eb73b8db9ea4b71ceb38d62f3749330a094
Niroj Naik <nirojnaik12@gmail.com>
2023-12-06 13:31:27 +0000

    new_changes=uses of getName and setName

--- Dec5_0.java
+++ Dec5_0.java
@@ -1,11 +1,20 @@
 public class Dec5_0
 {
     private String name= "are bhai how r u";
-    public String gateName(){
+    public String getName(){
         return name;
+
+    }
+    public void setName(String newName){
+        this.name=newName;
     }
     public static void main(String[]args){
         System.out.println("hello world");
+        Dec5_0 myObj= new Dec5_0();
+        System.out.println(myObj.getName());
+        myObj.setName("gone");
+        System.out.print(myObj.getName());
+
     }
         
     
