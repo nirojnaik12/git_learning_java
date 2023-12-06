f09971bacf7ff362c8d5261b62f7d26d7c63b809
Niroj Naik <nirojnaik12@gmail.com>
2023-12-06 13:20:58 +0000

    its a test file

---
+++ Dec5_0.java
@@ -0,0 +1,12 @@
+public class Dec5_0
+{
+    private String name= "are bhai how r u";
+    public String gateName(){
+        return name;
+    }
+    public static void main(String[]args){
+        System.out.println("hello world");
+    }
+        
+    
+}
