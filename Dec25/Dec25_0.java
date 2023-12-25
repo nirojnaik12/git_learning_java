f344f1693bb13c29a1a74a58f2ead258f0fd7535
Niroj Naik <nirojnaik12@gmail.com>
2023-12-25 10:48:15 +0000

    it might be last commitment of the year

---
+++ Dec25/Dec25_0.java
@@ -0,0 +1,14 @@
+package Dec25;
+
+import java.util.Scanner;
+public class Dec25_0{
+    public static void main(String[] args) {
+        try (Scanner myObj = new Scanner(System.in)) {
+            System.out.println("enter your num :");
+            String userName=myObj.nextLine();
+            System.out.println(userName);
+        }
+
+        
+    }
+}
