7de96b2cd925b5d92a6540028397fdc86cc94b4e
Niroj Naik <nirojnaik12@gmail.com>
2023-12-25 11:44:34 +0000

    new changes of new file 1

--- Dec25/Dec25_1.java
+++ Dec25/Dec25_1.java
@@ -1,5 +1,13 @@
 package Dec25;
 
+import java.time.LocalDate;
+
+
 public class Dec25_1 {
+    public static void main(String[] args) {
+        LocalDate myObj = LocalDate.now();
+
+        System.out.println(myObj);
+    }
     
 }
