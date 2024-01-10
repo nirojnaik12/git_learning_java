1622c2cc577655a226cf9fb1945ddec1494cb0a4
Niroj Naik <nirojnaik12@gmail.com>
2024-01-10 05:03:16 +0000

    its first commitment of the yr 2024

---
+++ Dec27/Dec27_0.java
@@ -0,0 +1,10 @@
+package Dec27;
+import java.time.LocalDate;
+
+public class Dec27_0 {
+    public static void main(String[] args) {
+        LocalDate myObj=LocalDate.now();
+        System.out.println(myObj);
+    }
+    
+}
