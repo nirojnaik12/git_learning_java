2b98ad80348300eb6da914c7b2f5c21d05142c00
Niroj Kumar Naik <nirojnaik12@gmail.com>
2024-03-11 09:25:51 +0530

    i learnt about java constructor

--- Newport6.java
+++ Newport6.java
@@ -1,7 +1,24 @@
 public class Newport6 {
-    int x=33;
+    int x;//its an attribute
+    int y;
+
+    //creating a class constructur and assigning a value to the attribute
+    public Newport6() {
+        x=5;
+    }
+
+    public Newport6(int Z){
+        y=z;
+    }
 
     public static void main(String[] args) {
 
+
+        Newport6 myObj = new Newport6();
+        System.out.println(myObj.x);
+        Newport6 myObj1 = new Newport6(33);
+        System.out.println(myObj1.y);
+
     }
 }
+
